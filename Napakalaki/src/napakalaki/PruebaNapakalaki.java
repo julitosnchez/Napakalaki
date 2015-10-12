01e8fbcb6ffa3e7c7c07305adf7b59219290cff5
JulitoSnchez <julio_santafe@hotmail.es>
2015-10-12 16:02:48 +0200

    Sesion 1--PDOO ugr (Napakalaki in process).

--- Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -22,16 +22,42 @@ public class PruebaNapakalaki {
        Prize prize;
        
        //Comenzamos a añadir monstruos
-       //'EL REY ROSA'
+       
+       //EL REY ROSA
        bc =  new BadConsequence("Pierdes 5 niveles y 3 tesoros visibles",5, 3, 0);
        prize = new Prize(4,2);
-       
        monstruos.add(new Monster("El rey rosa",13,bc,prize));
+       
        //ÁNGELES DE LA NOCHE IBICENCA
        bc = new BadConsequence("Te atrapan para llevarte de fiesta y te dejan caer en mitad del vuelo. Descarta 1 mano visible y 1 mano oculta",0,new ArrayList(Arrays.asList(TreasureKind.ONEHAND)),new ArrayList(Arrays.asList(TreasureKind.ONEHAND)));
        prize = new Prize(4,1);
-       
        monstruos.add(new Monster("Ángeles de la noche ibicenca",14,bc,prize));
+       
+       //3 BYAKHEES DE BONANZA
+       bc = new BadConsequence("Pierdes tu armadura visible y otra oculta",0,new ArrayList(Arrays.asList(TreasureKind.ARMOR)),new ArrayList(Arrays.asList(TreasureKind.ARMOR)));
+       prize = new Prize(2,1);
+       monstruos.add(new Monster("3 Byakhees de bonanza",8,bc,prize));
+       
+       //CHIBITHULHU
+       bc = new BadConsequence("Embobados con el lindo priminegio te descartas de tu casco visible",0,new ArrayList(Arrays.asList(TreasureKind.HELMET)),null);
+       prize = new Prize(1,1);
+       monstruos.add(new Monster("Chibithulhu",2,bc,prize));
+       
+       //EL SOPOR DE DUNWICH
+       bc = new BadConsequence("El primordial bostezo contagioso.Pierdes el calzado visible",0,new ArrayList(Arrays.asList(TreasureKind.SHOES)),null);
+       prize = new Prize(1,1);
+       monstruos.add(new Monster("El sopor de Dunwich",2,bc,prize));
+       
+       //EL GORRÓN EN EL UMBRAL
+       bc = new BadConsequence("Pierdes todos tus tesoros visibles",0,new ArrayList(Arrays.asList(TreasureKind.ARMOR,TreasureKind.BOTHHANDS,TreasureKind.HELMET,TreasureKind.ONEHAND,TreasureKind.SHOES)),null);
+       prize = new Prize(3,1);
+       monstruos.add(new Monster("El gorrón en el umbral",10,bc,prize));
+       
+       //H.P.Munchcraft
+       bc = new BadConsequence("Pierdes la armadura visible",0,new ArrayList(Arrays.asList(TreasureKind.ARMOR)),null);
+       prize = new Prize(2,1);
+       monstruos.add(new Monster("H.P.Munchcraft",6,bc,prize));
+       
        
        
     }
