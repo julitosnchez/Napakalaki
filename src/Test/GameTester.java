120cb0c5f47c9c96c15c71acb594ea2d79fd36a9
julitosnchez <julitosnchez@julitosnchez-nigga>
2015-11-25 21:14:57 +0100

    Merge origin/master
    Conflicts:
    	lib/main.PruebaNapakalakiR.rb
    	lib/monster.rb

--- src/Test/GameTester.java
+++ src/Test/GameTester.java
@@ -140,7 +140,7 @@ public class GameTester {
     capture = in.nextLine();
     try {
       option = Integer.valueOf(capture);
-        System.out.println("ESTE ES EL PROBLEMA "+howMany +option +" a veeeer...");
+        System.out.println("ESTE ES EL PROBLEMA "+howMany +Command.GoBack.menu +" a veeeer...");
       if (option < Command.GoBack.menu || option > howMany) { // no se ha escrito un entero en el rango permitido
         validInput = false;
       }
