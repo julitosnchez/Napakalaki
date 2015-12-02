a6b4a3dbaccc542e48db606ee31d2131da4f4438
JULIO <JULIO@JULIO-PC>
2015-12-02 10:35:46 +0100

    Merge origin/master
    Conflicts:
    	lib/card_dealer.rb
    	lib/dice.rb

---
+++ src/napakalaki/CultistPlayer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author JULIO
+ */
+public class CultistPlayer extends Player {
+    private static int totalCultistPlayers = 0;
+    private Cultist myCultistCard;
+    
+    public CultistPlayer(Player p,Cultist c){
+        
+    }
+
+    private Treasure giveMeATreasure(){
+        
+    }
+    
+    private boolean canYouGiveMeATreasure(){
+        
+    }
+    
+    public static int getTotatlCultistPlayers(){
+        
+    }
+    
+}
